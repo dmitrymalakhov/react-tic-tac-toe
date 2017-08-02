fcd4d2e3ed8a11e7826bedd1213ca1d69e97b3f3
Dmitry Malakhov <dmitrii.malakhov@gmail.com>
2017-08-02 08:27:19 +0300

    some fix

--- components/Cell/index.js
+++ components/Cell/index.js
@@ -4,11 +4,30 @@
 
 'use strict';
 
-import React from 'react';
+import React, { PureComponent } from 'react';
 import CellStyled from './styled/CellStyled';
 
-export const Cell = props => (
-  <CellStyled>
-    {props.children}
-  </CellStyled>
-);
+export class Cell extends PureComponent {
+  _handleClick = () => {
+    const { num, rowNum } = this.props;
+    this.props.onClick(num, rowNum);
+  }
+
+  _renderSymbol() {
+    const { mode } = this.props;
+
+    if (mode === 1)
+      return 'o';
+
+    if (mode === -1)
+      return 'x';
+  }
+
+  render() {
+    return (
+      <CellStyled onClick={this._handleClick}>
+        {this.props.children}
+      </CellStyled>
+    );
+  }
+}
