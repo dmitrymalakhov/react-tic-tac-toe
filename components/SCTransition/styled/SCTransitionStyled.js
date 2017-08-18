a5bce766c21cd07bd5cce7a493eb07c8afae45fc
Dmitry Malakhov <dmitrii.malakhov@gmail.com>
2017-08-18 19:36:20 +0300

    removed the stupid npm, added sc keyframes

--- components/SCTransition/styled/SCTransitionStyled.js
+++ components/SCTransition/styled/SCTransitionStyled.js
@@ -4,28 +4,30 @@
 
 'use strict';
 
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+`;
+
+const fadeOut = keyframes`
+  0% { opacity: 1; }
+  100% { opacity: 0; }
+`;
 
 const animation = ({ transitionOut }) => {
   const animationType = transitionOut
-    ? 'transitionOut'
-    : 'transitionIn';
+    ? fadeOut
+    : fadeIn;
 
   return `
-    animation: ${animationType} 2s ease-in-out;
+    animation: ${animationType} 1s ease-in-out;
   `;
 };
 
+
+
 export const SCTransitionStyled = styled.div`
   ${animation}
-
-  @keyframes transitionIn {
-    0% { opacity: 0; }
-    100% { opacity: 1; }
-  }
-
-  @keyframes transitionOut {
-    0% { opacity: 1; }
-    100% { opacity: 0; }
-  }
 `;
