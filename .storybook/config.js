b69e09c4c3b4ecc5151b516f230bae185a16159e
Dmitry Malakhov <dmitrii.malakhov@gmail.com>
2017-09-12 23:04:57 +0300

    react 15 rollback

--- .storybook/config.js
+++ .storybook/config.js
@@ -7,7 +7,7 @@
 import { addDecorator, configure } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 
-const req = require.context('../app/components', true, /stories\.js$/);
+const req = require.context('../client/components', true, /stories\.js$/);
 const loadStories = () => req.keys().forEach(filename => req(filename));
 
 addDecorator((story, context) => withInfo('common info')(story)(context));
--- __mocks__/react.js
+++
@@ -1,6 +0,0 @@
-global.window = global;
-window.addEventListener = () => {};
-
-window.requestAnimationFrame = () => {
-  throw new Error('requestAnimationFrame is not supported in Node');
-};
