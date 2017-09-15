6512f6fc5284f69bd6d05c5362bc9b87e0a46df0
Dmitry Malakhov <dmitrii.malakhov@gmail.com>
2017-09-15 09:20:16 +0300

    some fix

--- server/data/resolvers/Mutation.js
+++ server/data/resolvers/Mutation.js
@@ -2,7 +2,7 @@ const pubsub = require('./pubsub');
 const { addStatisticsPlayedGame } = require('../database');
 
 const Mutation = {
-  addStatistic: (root, { message: { score } }) => {
+  addStatistic: (root, { score }) => {
     const entry = addStatisticsPlayedGame('p1', 'p2', 0, score);
 
     pubsub.publish('statisticAdded', JSON.stringify({ entry }));
